294ae28c9d6bf5a40575713eedd61a655142bff2
clbokea <clbo@kea.dk>
2018-08-31 09:13:30 +0200

    fff

---
+++ Bird.java
@@ -0,0 +1,18 @@
+public class Bird {
+
+
+        String color;
+        int location;
+        int size;
+        int  speed;
+        String sound;
+        
+        
+        public void moveForward(){
+        
+         
+        }
+         
+
+
+}
---
+++ Pig.java
@@ -0,0 +1,27 @@
+public class Pig {
+
+        String shape;
+        String color;
+        int loaction;
+        String sound;
+        int movement;
+        String state;
+        
+        
+        public void animate() {
+        
+        }
+        
+        public void beKilled() {
+        
+        }
+        
+        public void create(){
+        
+        }
+        
+        public void makeSound () {
+        
+        }
+
+}
